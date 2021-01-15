22472d93dbc3d4ddf4f731d9c64e0c64e39605ca
marek <marek.michalowski@gmail.com>
2021-01-15 14:07:21 +0100

    Add new members
    	adding new members and getters and setters to Student class
    	and constructor.

--- src/com/luv2code/jsf/hello/Student.java
+++ src/com/luv2code/jsf/hello/Student.java
@@ -1,22 +1,39 @@
 package com.luv2code.jsf.hello;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
-@ManagedBean
+
+@ManagedBean()
 public class Student {
+	
+	
+	private int freePasses;
 	
 	private String firstName;
 	private String lastName;
 	private String country;
 	private String favoriteProgrammingLanguage;
-	
+	private String email;
+	private String postalCode;
+	private String phoneNumber;
+	private String studentCode;
+
 	private List<String> countryOptions;
 	private List<String> languageOptions;
 	
 	private List<String> favoriteProgrammingLanguages;
+	
+	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+	
 	
 	public Student() {
 		countryOptions = new ArrayList<String>();
@@ -52,6 +69,16 @@ public class Student {
 		
 	}
 	
+	public Student(String firstName, String lastName, String country, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.country = country;
+		this.email = email;
+	}
+
+
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -74,6 +101,46 @@ public class Student {
 	
 	public void setCountry(String country) {
 		this.country = country;
+	}
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getStudentCode() {
+		return studentCode;
+	}
+
+	public void setStudentCode(String studentCode) {
+		this.studentCode = studentCode;
 	}
 	
 	public String getFavoriteProgrammingLanguage() {
@@ -99,5 +166,45 @@ public class Student {
 	public void setFavoriteProgrammingLanguages(List<String> favoriteProgrammingLanguages) {
 		this.favoriteProgrammingLanguages = favoriteProgrammingLanguages;
 	}
+
+	public SimpleDateFormat getSimpleDateFormat() {
+		return simpleDateFormat;
+	}
+
+	public void setSimpleDateFormat(SimpleDateFormat simpleDateFormat) {
+		this.simpleDateFormat = simpleDateFormat;
+	}
 	
+	
+	public String getCurrentTime() {
+		return simpleDateFormat.format(Calendar.getInstance().getTime());
+	}
+	
+	
+	/*
+	 * method to custom validation
+	 */
+	public void validateTheStudentCode(FacesContext context, UIComponent component,
+										Object value) throws ValidatorException {
+		
+		if ( value == null ) {
+			return;
+		}
+		
+		String data = value.toString();
+		
+		// Student code must start with PK if not, throw exception
+		if ( !data.startsWith("PK")) {
+			FacesMessage message = new FacesMessage("Student code must start with PK");
+			throw new ValidatorException(message);
+		}
+		
+		
+	}
+
 }
+
+
+
+
+
